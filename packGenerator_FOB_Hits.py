10fa2c246c0abb51200f3f972990fd79f8ff7d91
altJ4CKT <jacktolley516@gmail.com>
2025-04-11 12:05:42 +0100

    Add files via upload

---
+++ packGenerator_FOB_Hits.py
@@ -0,0 +1,341 @@
+import os
+from _musicQuizSongGenerator import Generator
+
+gen: Generator = Generator(os.path.dirname(os.path.realpath(__file__))+"/Quiz Songs.db")
+
+packId = gen.CreatePack("FOB Hits", "TWitham")
+
+gen.InsertSong("Dance Dance", ["Fall Out Boy"], "From Under the Cork Tree", """She says, she's no good with words but I'm worse
+Barely stuttered out a joke of a romantic stuck to my tongue
+Weighed down with words too overdramatic
+Tonight it's "It can't get much worse"
+Vs. "No one should ever feel like"
+I'm two quarters and a heart down
+And I don't wanna forget how your voice sounds
+These words are all I have so I'll write them
+So you need them just to get by
+Dance, dance
+We're falling apart to half time
+Dance, dance
+And these are the lives you love to lead
+Dance, this is the way they'd love
+If they knew how misery loved me
+You always fold just before you're found out
+Drink up it's last call
+Last resort, but only the first mistake, and I
+I'm two quarters and a heart down
+And I don't want to forget how your voice sounds
+These words are all I have so I'll write them
+So you need them just to get by
+Why don't you show me a little bit of spine
+You've been saving for his mattress, love
+Dance, dance
+We're falling apart to half time
+Dance, dance
+And these are the lives you love to lead
+Dance, this is the way they'd love
+If they knew how misery loved me
+Why don't you show me a little bit of spine
+You've been saving for his mattress (Mattress, mattress)
+I only want sympathy in the form of you
+Crawling into bed with me
+Dance, dance
+We're falling apart to half time
+Dance, dance
+And these are the lives you love to lead
+Dance, this is the way they'd love (Way they'd love)
+Dance, this is the way they'd love (Way they'd love)
+Dance, this is the way they'd love
+If they knew how misery loved me
+Dance, dance
+Dance, dance
+Dance, dance
+Dance, dance""", [packId, 1, 7])
+
+
+
+gen.InsertSong("I Don't Care", ["Fall Out Boy"], "Folie à Deux", """Ohh
+Ohhh
+Say my name and his in the same breath
+I dare you to say they taste the same
+Let the leaves fall off in the summer
+And let December glow in flames
+Erase myself and let go
+Start it over again in Mexico
+These friends, they don't love you
+They just love the hotel suites, now
+I don't care what you think
+As long as it's about me
+The best of us can find happiness in misery
+I said I don't care what you think
+As long as it's about me
+The best of us can find happiness in misery
+Ohh
+Ohh
+Oh take a chance let your body get a tolerance
+I'm not a chance, put a heat wave in your pants
+Pull a breath like another cigarette
+Pawnshop heart trading up (trading up)
+On the oracle in my chest,
+Let the guitar scream like a fascist
+Sweat it out, shut your mouth
+Free love on the streets but
+In the alley it ain't that cheap, now
+I don't care what you think
+As long as it's about me
+The best of us can find happiness in misery
+I said I don't care what you think
+As long as it's about me
+The best of us can find happiness in misery
+Ohh
+Ohhh
+I said, I don't care just what you think
+as long as it's about me
+I said I don't care just what you think
+As long as it's about me
+I said I don't care (I don't care)
+I said I don't care (I don't care)
+I said I (I) don't (don't) care (care)
+I (I) don't (don't) care (no I don't)
+I don't care, (I said)
+I don't care, (I said)
+I (I) don't (don't) care (care)
+I don't care what you think
+As long as it's about me
+The best of us can find happiness in misery
+I said I don't care what you think
+As long as it's about me
+The best of us can find happiness in misery""", [packId, 12, 6])
+
+
+
+gen.InsertSong("Grand Theft Autumn/Where Is Your Boy", ["Fall Out Boy"], "Take This to Your Grave", """Where is your boy tonight?
+I hope he is a gentleman
+And maybe he won't find out what I know
+You were the last good thing about this part of town
+When I wake up
+I'm willing to take my chances on
+The hope I forget that you hate him more than you notice
+I wrote this for you, for you, so
+You need him, I could be him
+I could be an accident but I'm still tryin'
+And that's more than I can say for him
+Where is your boy tonight?
+I hope he is a gentleman
+And maybe he won't find out what I know
+You were the last good thing about this part of town
+Someday I'll appreciate in value
+Get off my ass and call you
+The mean time, I'll sport my brand new fashion
+Of waking up with pants on at four in the afternoon
+You need him, I could be him
+I could be an accident but I'm still tryin'
+And that's more than I can say for him
+One, two, three, four
+Where is your boy tonight
+I hope he is a gentleman
+And maybe he won't find out what I know
+You were the last good thing about this part of town
+Won't find out
+He won't find out
+Won't find out
+He won't find out
+Where is your boy tonight?
+I hope he is a gentleman
+Maybe he won't find out what I know
+You were the last good thing about this part of town
+Where is your boy tonight?
+I hope he is a gentleman
+Maybe he won't find out what I know
+You were the last good thing about this part of town""", [packId, 17, 8])
+
+gen.InsertSong("This Ain't a Scene, It's an Arms Race", ["Fall Out Boy"], "Infinity on High", """I am an arms dealer fitting you with
+Weapons in the form of words
+And don't really care which side wins
+As long as the room keeps singing
+That's just the business I'm in
+This ain't a scene, it's a goddamn arms race
+This ain't a scene, it's a goddamn arms race
+This ain't a scene, it's a goddamn arms race
+I'm not a shoulder to cry on
+But I digress
+I'm a leading man
+And the lies I weave are oh so intricate
+Oh so intricate
+I'm a leading man
+And the lies I weave are oh so intricate
+Oh so intricate, yeah
+I wrote the gospel on giving up
+Look pretty sinking
+But the real bombshells have already sunk
+Prima Donnas of the gutter
+At night we're painting your trash gold
+While you sleep
+Crashing not like hips or cars
+But more like p-p-p-parties
+This ain't a scene, it's a goddamn arms race
+This ain't a scene, it's a goddamn arms race
+This ain't a scene, it's a goddamn arms race
+Bandwagon's full
+Please catch another
+I'm a leading man
+And the lies I weave are oh so intricate
+So intricate
+I'm a leading man
+Lies I weave are oh so intricate
+So intricate
+Yeah
+Woah, oh, woah oh, oh, oh
+All the boys who the dancefloor didn't love
+And all the girls whose lips couldn't move
+Fast enough
+Sing 'til your lungs give out
+'Cause this ain't a scene, it's a goddamn arms race
+This ain't a scene, it's a goddamn arms race
+(Now you)
+This ain't a scene, it's a goddamn arms race
+This ain't a scene, it's a goddamn arms race
+(Sing it out loud)
+(This ain't a scene, it's a goddamn arms race)
+(This ain't a scene) Oh
+It's a goddamn arms race
+I'm a leading man
+The lies I weave are oh so intricate
+So intricate
+I'm a leading man
+And the lies I weave are oh so intricate
+So intricate""", [packId, 14, 4])
+
+gen.InsertSong("Just One Yesterday", ["Fall Out Boy", "Foxes"], "Save Rock and Roll", """I thought of angels choking on their halos
+Get them drunk on rose water
+See how dirty I can get them, pulling out their fragile teeth
+And clip their tiny wings
+Anything you say can and will be held against you
+So only say my name, it will be held against you
+Anything you say can and will be held against you
+So only say my name
+If heaven's grief brings hell's rain
+Then I'd trade all my tomorrows for just one yesterday
+(I know I'm bad news)
+For just one yesterday
+(I saved it all for you) oh
+I want to teach you a lesson in the worst kind of way
+Still, I'd trade all my tomorrows for just one yesterday
+(I know I'm bad news)
+For just one yesterday
+(I saved it all for you)
+For just one yesterday
+Letting people down is my thing, baby, find yourself a new gig
+This town ain't big enough for two of us
+I don't have the right name o-o-or the right looks
+But I have twice the heart
+Anything you say can and will be held against you
+So only say my name, it will be held against you
+Anything you say can and will be held against you
+So only say my name (name)
+If heaven's grief brings hell's rain
+Then I'd trade all my tomorrows for just one yesterday
+(I know I'm bad news)
+For just one yesterday
+(I saved it all for you)
+I want to teach you a lesson in the worst kind of way
+Still, I'd trade all my tomorrows for just one yesterday
+(I know I'm bad news)
+For just one yesterday
+(I saved it all for you)
+For just one yesterday
+If I spilled my guts
+The world would never look at you the same way
+And now I'm here to give you all of my love
+So I can watch your face as I take it all away, away, away, yeah
+If heaven's grief brings hell's rain (yeah)
+Then I'd trade all my tomorrows for just one yesterday
+(I know I'm bad news) yeah
+For just one yesterday
+(I saved it all for you)
+I want to teach you a lesson in the worst kind of way
+Still, I'd trade all my tomorrows for just one yesterday
+(I know I'm bad news)
+For just one yesterday
+(I saved it all for you)
+For just one yesterday""", [packId, 1, 2])
+
+gen.InsertSong("The Last Of The Real Ones", ["Fall Out Boy"], "MANIA", """I was just an only child of the universe
+And then I found you
+And then I found you
+You are the sun and I am just the planets
+Spinning around you
+Spinning around you
+You were too good to be true
+Gold plated
+But what's inside you
+But what's inside you
+I know this whole damn city thinks it needs you
+But not as much as I do
+As much as I do, yeah
+'Cause you're the last of a dying breed
+Write our names in the wet concrete
+I wonder if your therapist knows everything about me
+I'm here in search of your glory
+There's been a million before me
+That ultra-kind of love
+You never walk away from
+You're just the last of the real ones
+You're just the last of the real ones
+You're just the last of the real ones
+(You're just the last of the real ones)
+You're just the last of the real ones
+(You're just the last of the real ones)
+You're just the last of the real ones
+I am a collapsing star with tunnel vision
+But only for you
+But only for you
+My head is stripped just like a screw that's been tightened too many times
+When I think of you
+When I think of you
+I will shield you from the waves
+If they find you
+I will protect you
+I will protect you
+Just tell me, tell me, tell me, I
+I am the only one
+Even if it's not true
+Even if it's not true, yeah
+'Cause you're the last of a dying breed
+Write our names in the wet concrete
+I wonder if your therapist knows everything about me
+I'm here in search of your glory
+There's been a million before me
+That ultra-kind of love
+You never walk away from
+You're just the last of the real ones
+You're just the last of the real ones
+You're just the last of the real ones
+(You're just the last of the real ones)
+You're just the last of the real ones
+(You're just the last of the real ones)
+You're just the last of the real ones
+I'm here at the beginning of the end
+Oh, the end of infinity with you
+I'm here at the beginning of the end
+Oh, the end of infinity with you
+I'm done with having dreams
+The thing that I believe
+Oh, you drain all the fear from me
+I'm done with having dreams
+The thing that I believe
+You drain the fear from me
+'Cause you're the last of a dying breed
+Write our names in the wet concrete
+I wonder if your therapist knows everything about me
+I'm here in search of your glory
+There's been a million before me
+That ultra-kind of love
+You never walk away from
+You're just the last of the real ones
+You're just the last of the real ones
+You're just the last of the real ones
+(You're just the last of the real ones)
+You're just the last of the real ones
+(You're just the last of the real ones)
+You're just the last of the real ones""", [packId, 16, 6])
