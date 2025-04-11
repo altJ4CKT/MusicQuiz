2cbc53855431060849085e1dc63b6e27209fa81a
altJ4CKT <jacktolley516@gmail.com>
2025-04-11 12:05:20 +0100

    Add files via upload

---
+++ packGenerator_2010s_Pop.py
@@ -0,0 +1,316 @@
+import os
+from _musicQuizSongGenerator import Generator
+
+gen: Generator = Generator(os.path.dirname(os.path.realpath(__file__))+"/Quiz Songs.db")
+
+packId = gen.CreatePack("2010's Pop", "JWitham")
+
+gen.InsertSong("The Nights", ["Avicii"], "Stories", """(Hey) Once upon a younger year
+When all our shadows disappeared
+The animals inside came out to play
+(Hey) Went face to face with all our fears
+Learned our lessons through the tears
+Made memories we knew would never fade
+One day my father he told me
+"Son, don't let it slip away"
+He took me in his arms, I heard him say (hey)
+"When you get older
+Your wild heart will live for younger days
+Think of me if ever you're afraid"
+He said, "One day, you'll leave this world behind
+So live a life you will remember"
+My father told me when I was just a child
+These are the nights that never die
+My father told me
+(Hey, hey)
+(Hey, hey)
+(Hey, hey)
+(Hey, hey)
+(Hey, hey)
+(Hey, hey)
+(Hey, hey)
+When thunder clouds start pouring down
+Light a fire they can't put out
+Carve your name into those shinning stars
+He said, "Go venture far beyond the shores
+Don't forsake this life of yours
+I'll guide you home no matter where you are"
+One day my father, he told me
+"Son, don't let it slip away"
+When I was just a kid I heard him say
+"When you get older
+Your wild heart will live for younger days
+Think of me if ever you're afraid"
+He said, "One day, you'll leave this world behind
+So live a life you will remember"
+My father told me when I was just a child
+These are the nights that never die
+My father told me
+(Hey, hey)
+(Hey, hey)
+(Hey, hey)
+"These are the nights that never die"
+My father told me
+(Hey, hey)
+(Hey, hey)
+Ooh-whoa, oh-oh-oh-oh-oh-oh
+(Hey, hey)
+Oh-oh (hey)
+(Hey, hey)
+(Hey, hey)
+Ooh-whoa, oh-oh-oh-oh-oh-oh
+(Hey, hey)
+Oh-oh (hey)
+My father told me""", [packId, 12, 7]) # remember
+
+
+gen.InsertSong("There's Nothing Holdin' Me Back", ["Shawn Mendes"], "Illuminate", """I wanna follow where she goes
+I think about her and she knows it
+I wanna let her take control
+'Cause every time that she gets close, yeah
+She pulls me in enough to keep me guessin', hm
+And maybe I should stop and start confessin'
+Confessin', yeah
+Oh, I've been shakin', I love it when you go crazy
+You take all my inhibitions
+Baby, there's nothing holdin' me back
+You take me places that tear up my reputation
+Manipulate my decisions
+Baby, there's nothing holdin' me back
+There's nothing holdin' me back
+There's nothing holdin' me back
+She says that she's never afraid
+Just picture everybody naked
+She really doesn't like to wait
+Not really into hesitation
+Pulls me in enough to keep me guessin', whoa
+And maybe I should stop and start confessin'
+Confessin', yeah
+Oh, I've been shakin', I love it when you go crazy
+You take all my inhibitions
+Baby, there's nothing holdin' me back
+You take me places that tear up my reputation
+Manipulate my decisions
+Baby, there's nothing holdin' me back
+There's nothing holdin' me back
+'Cause if we lost our minds and we took it way too far
+I know we'd be alright, I know we would be alright
+If you were by my side and we stumbled in the dark
+I know we'd be alright, I know we would be alright
+'Cause if we lost our minds and we took it way too far
+I know we'd be alright, I know we would be alright
+If you were by my side and we stumbled in the dark
+I know we'd be alright, we would be alright
+Oh, I've been shakin', I love it when you go crazy
+You take all my inhibitions
+Baby, there's nothing holdin' me back
+You take me places that tear up my reputation
+Manipulate my decisions
+Baby, there's nothing holdin' me back, oh-whoa
+There's nothing holdin' me back
+I feel so free when you're with me, baby
+Baby, there's nothing holdin' me back""", [packId, 24, 2]) # nothing
+
+
+
+gen.InsertSong("Somebody That I Used To Know", ["Gotye", "Kimbra"], "Making Mirrors", """Now and then I think of when we were together
+Like when you said you felt so happy you could die
+Told myself that you were right for me
+But felt so lonely in your company
+But that was love and it's an ache I still remember
+You can get addicted to a certain kind of sadness
+Like resignation to the end, always the end
+So when we found that we could not make sense
+Well you said that we would still be friends
+But I'll admit that I was glad it was over
+But you didn't have to cut me off
+Make out like it never happened and that we were nothing
+And I don't even need your love
+But you treat me like a stranger and that feels so rough
+No, you didn't have to stoop so low
+Have your friends collect your records and then change your number
+I guess that I don't need that though
+Now you're just somebody that I used to know
+Now you're just somebody that I used to know
+Now you're just somebody that I used to know
+Now and then I think of all the times you screwed me over
+But had me believing it was always something that I'd done
+But I don't wanna live that way
+Reading into every word you say
+You said that you could let it go
+And I wouldn't catch you hung up on somebody that you used to know
+But you didn't have to cut me off
+Make out like it never happened and that we were nothing
+And I don't even need your love
+But you treat me like a stranger and that feels so rough
+No, you didn't have to stoop so low
+Have your friends collect your records and then change your number
+I guess that I don't need that though
+Now you're just somebody that I used to know
+Somebody (I used to know)
+(Somebody) now you're just somebody that I used to know
+Somebody (I used to know)
+(Somebody) now you're just somebody that I used to know
+I used to know, that I used to know, I used to know somebody""", [packId, 5, 9]) # sadness
+
+
+gen.InsertSong("It's Time", ["Imagine Dragons"], "Night Visions", """So this is what you meant
+When you said that you were spent
+And now it's time to build from the bottom of the pit, right to the top
+Don't hold back
+Packing my bags and giving the Academy a rain check
+I don't ever want to let you down
+I don't ever want to leave this town
+'Cause after all
+This city never sleeps at night
+It's time to begin, isn't it?
+I get a little bit bigger but then I'll admit
+I'm just the same as I was
+Now don't you understand
+That I'm never changing who I am
+So this is where you fell
+And I am left to sell
+The path to heaven runs through miles of clouded hell right to the top
+Don't look back
+Turning to rags and giving the commodities a rain check
+I don't ever want to let you down
+I don't ever want to leave this town
+'Cause after all
+This city never sleeps at night
+It's time to begin, isn't it?
+I get a little bit bigger but then I'll admit
+I'm just the same as I was
+Now don't you understand
+That I'm never changing who I am
+It's time to begin, isn't it?
+I get a little bit bigger but then I'll admit
+I'm just the same as I was
+Now don't you understand
+That I'm never changing who I am
+This road never looked so lonely
+This house doesn't burn down slowly
+To ashes
+To ashes
+It's time to begin, isn't it?
+I get a little bit bigger but then I'll admit
+I'm just the same as I was
+Now don't you understand
+That I'm never changing who I am
+It's time to begin, isn't it?
+I get a little bit bigger but then I'll admit
+I'm just the same as I was
+Now don't you understand
+That I'm never changing who I am""", [packId, 10, 5]) # bigger
+
+
+gen.InsertSong("Best Thing I Never Had", ["Beyoncé"], "4", """What goes around, comes back around, hey (my baby)
+What goes around, comes back around, (my) hey (my baby)
+I say, what goes around, comes back around, hey (my baby)
+What goes around, comes back around
+There was a time
+I thought that you did everything right
+No lies, no wrong
+Boy, I must've been out of my mind
+So when I think of the time that I almost loved you
+You showed your ass and I, I saw the real you
+Thank God, you blew it
+I thank God, I dodged the bullet
+I'm so over you
+So, baby, good lookin' out
+I wanted you bad
+I'm so through with that
+'Cause honestly, you turned out to be the (best thing I never had)
+You turned out to be the (best thing I never had)
+And I'm gon' always be the (best thing you never had)
+Oh, yeah, I bet it sucks to be you right now
+So sad, you're hurt
+Boohoo, oh, did you expect me to care?
+You don't deserve my tears
+I guess that's why they ain't there
+When I think that there was a time that I almost loved you
+You showed your ass and baby, yes, I saw the real you
+Thank God, you blew it
+I thank God, I dodged the bullet
+I'm so over you
+Baby, good lookin' out
+I wanted you bad
+I'm so through with that
+'Cause honestly, you turned out to be the (best thing I never had)
+I said, you turned out to be the (best thing I never had)
+Mm, and I'll never be the (best thing you never had)
+Oh, baby, I bet it sucks to be you right now
+I know you want me back
+It's time to face the facts that I'm the one that's got away
+Lord knows that it would take another place, another time
+Another world, another life
+Thank God, I found the good in goodbye
+I used to want you so bad
+I'm so through with that
+'Cause honestly, you turned out to be the (best thing I never had)
+Oh, you turned out to be the (best thing I never had)
+And I will always be the (best thing you never had)
+Ooh, best thing you never had
+I used to want you so bad
+I'm so through with that
+'Cause honestly, you turned out to be the (best thing I never had)
+Oh, you turned out to be the (best thing I never had)
+Ooh, I will never be the (best thing you never had)
+Oh, baby, I bet it sucks to be you right now
+(What goes around, comes back around)
+(What goes around, comes back around)
+Bet it sucks to be you right now
+(What goes around, comes back around)
+I bet it sucks to be you right now
+(What goes around, comes back around)
+I bet it sucks to be you right now (ooh)""", [packId, 9, 9])
+
+
+
+gen.InsertSong("My Number", ["Foals"], "Holy Fire", """You don't have my number
+We don't need each other now
+We don't need the city
+The creed or the culture now
+'Cause I feel, well, I feel alive
+I feel, well, I feel alive
+I feel the streets are not pulling me down
+So people of the city
+I don't need your counsel now
+And I don't need your good advice
+'Cause you don't have my lover's touch
+You don't have my number
+We don't need each other now
+The creed or the culture
+We can move beyond it now
+Now the wolf is knocking at my door
+Bang-bang, it asks for more
+Stand here, we stand tall
+We can move beyond these walls
+And I don't need your counsel
+And I don't need these city streets
+And I don't need that good advice
+'Cause we can move beyond it now
+You don't have my number
+We don't need each other now
+The creed or the culture
+You don't have my lover's touch
+'Cause I feel, well, I feel alive
+I feel, well, I feel alive
+I feel the streets are not far from here
+Can you even hear me?
+Do you even know my name?
+Can you see the ocean there?
+You don't have my lover's touch
+You don't have my number
+You don't have my number
+I wonder, can you hear me?
+And are you even listening now?
+'Cause you don't have my number
+And we don't need each other now
+And you can't steal my thunder
+'Cause you don't have my lover's touch
+You don't have my number
+And I don't need no one now
+And I don't need these city streets
+The creed or the culture now
+Now
+Now""", [packId, 2, 4]) # city
